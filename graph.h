1a2c6dc4fd11059d8348faa8f73b2d18bc76f0c6
Priyesh Patel <ppatel1849@sdsu.edu>
2025-05-15 10:27:56 -0700

    Graph constructor possibly working

--- graph.h
+++ graph.h
@@ -3,9 +3,22 @@
 
 class Graph {
 public:
-    Graph(int vertices);
+    Graph(int vertices) {
+        numVertices = vertices;
+        adjMatrix = new int*[numVertices];
+        for (int i = 0; i < numVertices; ++i) {
+            adjMatrix[i] = new int[numVertices];
+            for (int j = 0; j < numVertices; ++j) {
+                if (i == j)
+                    adjMatrix[i][j] = 0;
+                else
+                    adjMatrix[i][j] = -1;
+            }
+        }
+    };
     ~Graph();
-    void addEdge(int u, int v, int weight);
+    void addEdge(int u, int v, int weight){
+    }
     void primMST();  // Must print MST edges and total weight
 
 private:
